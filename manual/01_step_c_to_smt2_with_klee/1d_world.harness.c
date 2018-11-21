c00d9ad93a774fc3503b1a2c296db6d711ee2e4a
Kevin Laeufer <ekiwi@berkeley.edu>
2018-11-20 22:26:31 -0800

    [steps] in harness: change pos_in to pos

--- manual/01_step_c_to_smt2_with_klee/1d_world.harness.c
+++ manual/01_step_c_to_smt2_with_klee/1d_world.harness.c
@@ -14,7 +14,7 @@ void left_h() {
 }
 
 void right_h() {
-	klee_make_symbolic(&pos, sizeof(pos), "pos_in");
+	klee_make_symbolic(&pos, sizeof(pos), "pos");
 	bool success = right();
 	klee_dump_expr("success_n", success);
 	klee_dump_expr("pos_n", pos);
